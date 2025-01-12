e65730523bf43dc16403f8590efa23969d7192c8
PhilipeF <philipefdev@gmail.com>
2025-01-12 16:11:34 -0300

    Treinando fundamentos

--- calcularData/app.js
+++ calcularData/app.js
@@ -18,5 +18,5 @@ function calcularDiasEntreDatas(dia, mes, ano) {
   };
 }
 
-const resultado1 = calcularDiasEntreDatas(11, 1)
+const resultado1 = calcularDiasEntreDatas(12, 1)
 console.log('Resultado foi', resultado1.dias);
