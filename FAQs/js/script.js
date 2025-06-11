6f2651b55c85a5fc06d5edbdde090ff06b5387bc
PhilipeF <philipefdev@gmail.com>
2025-06-11 01:47:23 -0300

    Logica do FAQ ok - não permetindo que duas questões fiquem abertas ao mesmo tempo

--- FAQs/js/script.js
+++ FAQs/js/script.js
@@ -2,7 +2,15 @@ const faqItems = document.querySelectorAll(".faq-item");
 
 faqItems.forEach((item) => {
   const question = item.querySelector(".faq-question");
-  question.addEventListener('click', () => {
-    item.classList.toggle('active')
+  question.addEventListener("click", () => {
+    const isActive = item.classList.contains("active");
+
+    faqItems.forEach((el) => {
+      el.classList.remove("active");
+    });
+
+    if (!isActive) {
+      item.classList.add("active");
+    }
   });
 });
