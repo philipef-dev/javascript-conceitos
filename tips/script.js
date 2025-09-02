b82c787b167f2ab35b4178f38771bd9d66012c2d
rick <philipefdev@gmail.com>
2025-09-02 17:46:57 -0300

    Resolvendo um caso de uso

---
+++ tips/script.js
@@ -0,0 +1,5 @@
+// Vamos supor que eu tenha um Array de datas e quero fazer com um função me traga o mês atual 
+
+const meses = ['janeiro', 'fevereiro', 'marco', 'abril', 'maio', 'junho', 'julho', 'agosto', 'setembro', 'outubro', 'novembro', 'dezembro']
+
+console.log(meses)
